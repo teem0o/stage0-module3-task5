9de12bf55baa490bc06f02c5497102d959fccfcf
temo <chxaidzetemo@gmail.com>
2022-07-07 21:01:02 +0400

    hh

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1,second = 10,third = 100;
+        System.out.printf("%d%d%d",first,second,third);
+        System.out.println();
     }
 }
